a384afed9f714a9f9c7fbe17481fecf3d393a9c1
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-04-17 02:24:19 +0900

    modify: comment libiconv packages

--- modules/essentials/manifests/install.pp
+++ modules/essentials/manifests/install.pp
@@ -5,7 +5,7 @@ class essentials::install {
     'curl', 'bash',
     'gettext', 'gettext-base',
     'libxslt1.1', 'libxslt-dev', 'libxml2', 'libxml2-dev', 'libssl-dev', 'libffi-dev', 'libsqlite3-dev', 'libreadline6-dev', 'libgd2-xpm-dev',
-    'libiconv', 'libiconv-dev',
+#    'libiconv', 'libiconv-dev',
     'git-core', 'subversion',
     'lv',
     'unzip', 'lha',
